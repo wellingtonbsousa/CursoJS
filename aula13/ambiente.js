fd50837b82d23416f3d347023cd0d66b64825288
Wellington Barbosa e Sousa <wellington.b.sousa2014@gmail.com>
2025-06-06 18:21:57 -0300

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+/*
+while (c <= 6) { // Teste logico no inicio
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+}
+*/
+do { // Teste logico no final
+    console.log(`Passo ${c}`)
+    c++ // c = c + 1
+} while (c <= 6)
